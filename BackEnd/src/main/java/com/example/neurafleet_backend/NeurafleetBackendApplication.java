ae7755e08ee795185e23fbeaa6dafe767dc3485b
Energeticninjha <sreedharshini207@gmail.com>
2026-01-08 15:53:24 +0530

    Add Spring Boot backend files with MySQL and Security dependencies

---
+++ BackEnd/src/main/java/com/example/neurafleet_backend/NeurafleetBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.neurafleet_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NeurafleetBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NeurafleetBackendApplication.class, args);
+	}
+
+}
---
+++ BackEnd/src/test/java/com/example/neurafleet_backend/NeurafleetBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.neurafleet_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NeurafleetBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
